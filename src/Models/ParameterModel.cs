03f0c09ee5c1b25539bdb72c8438a23237ad39e3
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-04-11 16:04:10 +0200

    Removed unused properties

--- src/Models/ParameterModel.cs
+++ src/Models/ParameterModel.cs
@@ -1,22 +1,13 @@
 ﻿namespace WinDbgSymbolsCachingProxy.Models;
 
-public enum OutputFileFormat { Svg, Png }
-
-public enum ActionType { ShowHelp, CreateImage }
-
 public class ParameterModel
 {
     public ParameterModel()
     {
-        OutputFile = "out.svg";
-        OutputFileFormat = OutputFileFormat.Svg;
-        Action = ActionType.ShowHelp;
         Height = 22;
         LabelColor = "#333333ff";
         ResultColor = "#00ff00ff";
     }
-
-    public ActionType Action { get; set; }
 
     public string Label { get; set; }
     public string Result { get; set; }
@@ -24,9 +15,4 @@ public class ParameterModel
     public string LabelColor { get; set; }
 
     public int Height { get; set; }
-
-    public string OutputFile { get; set; }
-    public OutputFileFormat OutputFileFormat { get; set; }
-
-    public string HelpText { get; set; }
 }
