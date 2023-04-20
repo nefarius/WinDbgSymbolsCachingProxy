c1455e7cfc8d71fcf62b631930cacb705a06290c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-04-20 19:06:01 +0200

    Create Index2Endpoint.cs

---
+++ src/Endpoints/Index2Endpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+
+namespace WinDbgSymbolsCachingProxy.Endpoints;
+
+public sealed class Index2Endpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/download/symbols/index2.txt");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        return SendNotFoundAsync(ct);
+    }
+}
