82af5a44da33346bd64b1ea0f6963ba7487af48e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-04-21 01:28:27 +0200

    Added migrations

---
+++ src/Migrations/_002_FileToFileNameMigration.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Entities;
+
+using WinDbgSymbolsCachingProxy.Models;
+
+namespace WinDbgSymbolsCachingProxy.Migrations;
+
+public class _002_FileToFileNameMigration : IMigration
+{
+    public Task UpgradeAsync()
+    {
+        return DB.Update<SymbolsEntity>()
+            .Match(_ => true)
+            .Modify(b => b.Rename("File", "FileName"))
+            .ExecuteAsync();
+    }
+}
---
+++ src/Migrations/_003_GenerateIndexPrefix.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Entities;
+
+using WinDbgSymbolsCachingProxy.Models;
+
+namespace WinDbgSymbolsCachingProxy.Migrations;
+
+public class _003_GenerateIndexPrefix : IMigration
+{
+    public Task UpgradeAsync()
+    {
+        return DB.Update<SymbolsEntity>()
+            .Match(_ => true)
+            .WithPipelineStage("{$set:{IndexPrefix:{$concat:['$Symbol','/','$SymbolKey']}}}")
+            .ExecutePipelineAsync();
+    }
+}
