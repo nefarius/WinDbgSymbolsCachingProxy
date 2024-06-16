68733a021f1900552502f741df259a08373fd418
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-06-16 11:24:05 +0200

    Refined server version info

--- src/Endpoints/RootEndpoint.cs
+++ src/Endpoints/RootEndpoint.cs
@@ -39,18 +39,9 @@ public sealed class RootEndpoint : EndpointWithoutRequest<RootResponse>
 
         StringTable stringTable = peFile.Resources.VsVersionInfo!.StringFileInfo.StringTable.First();
 
-        string? productVersion = stringTable.ProductVersion;
-
-        if (productVersion is null)
-        {
-            _logger.LogError("Couldn't get product version");
-            await SendErrorsAsync(500, ct);
-            return;
-        }
-
         await SendOkAsync(new RootResponse
         {
-            ServerVersion = productVersion,
+            ServerVersion = stringTable.FileVersion,
             CachedSymbolsTotal = await DB.CountAsync<SymbolsEntity>(cancellation: ct),
             CachedSymbols404 = await DB.CountAsync<SymbolsEntity>(
                 s => s.NotFoundAt != null,
