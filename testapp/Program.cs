b1ef2e5407dc56ec0a10d538c52eeae6cac4b101
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-01-26 16:59:44 +0100

    Update Program.cs

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Diagnostics.CodeAnalysis;
+
 using Nefarius.Utilities.ExceptionEnricher;
 
 try
@@ -10,6 +12,7 @@ try
 catch (Exception ex)
 {
     // just a demo, do not do IRL ofc. ;)
+    // always use IHttpClientFactory or you will get socket exhaustion!
     using HttpClient client = new();
     client.BaseAddress = new Uri("http://192.168.2.12:5000");
 
@@ -23,11 +26,17 @@ catch (Exception ex)
 
 return;
 
+/*
+ * Some nested local functions to make a demo call stack
+ */
+
+[SuppressMessage("ReSharper", "UnusedParameter.Local")]
 void Foo(int firstArg = 1, int secondArg = 2)
 {
     Bar();
 }
 
+[SuppressMessage("ReSharper", "UnusedParameter.Local")]
 void Bar(params List<int> someArgs)
 {
     Baz();
