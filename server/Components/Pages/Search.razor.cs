18f6ef3f8f9c5474d3d2113f44cd71f9717dbd82
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-01-31 16:02:28 +0100

    Added quick filter

--- server/Components/Pages/Search.razor.cs
+++ server/Components/Pages/Search.razor.cs
@@ -7,9 +7,33 @@ namespace WinDbgSymbolsCachingProxy.Components.Pages;
 
 public partial class Search
 {
-    private string _searchString;
-    
     private IEnumerable<SymbolsEntity> _elements = new List<SymbolsEntity>();
+    private string _searchString;
+
+    private Func<SymbolsEntity, bool> QuickFilter => x =>
+    {
+        if (string.IsNullOrWhiteSpace(_searchString))
+        {
+            return true;
+        }
+
+        if (x.FileName.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+
+        if (x.IndexPrefix.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+
+        if (x.SymbolKey.Contains(_searchString, StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+
+        return false;
+    };
 
     protected override async Task OnInitializedAsync()
     {
