c254a1f2b31dc835c4cd84fe1e18748df888027b
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-07-30 02:15:20 +0200

    Update SymbolsEntityMapper.cs

--- server/Mappers/SymbolsEntityMapper.cs
+++ server/Mappers/SymbolsEntityMapper.cs
@@ -7,5 +7,7 @@ namespace WinDbgSymbolsCachingProxy.Mappers;
 [Mapper]
 public partial class SymbolsEntityMapper
 {
+#pragma warning disable RMG020
     public partial SymbolsEntityDto SymbolsEntityToSymbolsEntityDto(SymbolsEntity entity);
+#pragma warning restore RMG020
 }
